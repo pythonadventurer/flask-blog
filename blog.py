22bdf98fb349ed0f5b3311582c131d0deeb941c8
Rob Fowler <bluegator4@gmail.com>
2023-10-14 14:09:32 -0400

    added listing of blog posts

--- blog.py
+++ blog.py
@@ -4,7 +4,10 @@
 from flask import Flask, render_template, request, session, \
     flash, redirect, url_for, g
 
+from functools import wraps
+
 import sqlite3
+import pdb
 
 # configuration
 DATABASE = 'blog.db'
@@ -22,6 +25,17 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return test(*args, **kwargs)
+        else:
+            flash('You need to log in first.')
+            return redirect(url_for('login'))
+    return wrap
+
+
 @app.route('/',methods=['GET','POST'])
 def login():
     error = None
@@ -31,20 +45,30 @@ def login():
                 request.form['password'] != app.config['PASSWORD']:
             error = 'Invalid credentials. Please try again.'
             status_code = 401
+
         else:
             session['logged_in'] = True
             return redirect(url_for('main'))
-    return render_template('login.html')
+    return render_template('login.html',error=error), status_code
+
 
 @app.route('/main')
+@login_required
 def main():
-    return render_template('main.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0],post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html',posts=posts)
+
 
 @app.route('/logout')
 def logout():
     session.pop('logged_in',None)
     flash('You have logged out.')
     return redirect(url_for('login'))
+
+
 
 
 if __name__ == '__main__':

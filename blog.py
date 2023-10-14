26aea653a526c68a6a6439f0b0e1805d0d80b81e
Rob Fowler <bluegator4@gmail.com>
2023-10-14 13:22:06 -0400

    basic login and logout

--- blog.py
+++ blog.py
@@ -8,6 +8,10 @@ import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = b'c\xb7`\x8e\x8c\xf5\x1f\xd9-\x83Q\xa0w#\x1a:\xfe\xc3M\x02\x86y&\x9b'
+
 
 app = Flask(__name__)
 
@@ -18,13 +22,29 @@ app.config.from_object(__name__)
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def login():
+    error = None
+    status_code = 200
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+                request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid credentials. Please try again.'
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
     return render_template('login.html')
 
 @app.route('/main')
 def main():
     return render_template('main.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    flash('You have logged out.')
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
